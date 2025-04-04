9897ffed1f1a505aa455b74390db38f108062046
Erastus <emauliate17@gmail.com>
2025-04-04 22:05:49 +0700

    tests: add script User Registration in auth.test.ts

---
+++ tests/auth.test.ts
@@ -0,0 +1,27 @@
+import request from "supertest";
+import app from "../src/app";
+
+describe("User Registration", () => {
+  it("should register a new user", async () => {
+    const res = await request(app).post("/api/v1/auth/register").send({
+      name: "ucok",
+      email: "test200@example.com",
+      password: "password123",
+    });
+
+    expect(res.status).toBe(200);
+    expect(res.body.success).toBe(true);
+    expect(res.body.message).toBe("Registrasi berhasil");
+  });
+
+  it("should fail with an existing email", async () => {
+    const res = await request(app).post("/api/v1/auth/register").send({
+      name: "hehe",
+      email: "test@example.com",
+      password: "password123",
+    });
+
+    expect(res.status).toBe(400);
+    expect(res.body.success).toBe(false);
+  });
+});
