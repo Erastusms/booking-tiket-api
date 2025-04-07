28df47fe6601588aaf27fa49a25c9cf162df4e50
Erastus <emauliate17@gmail.com>
2025-04-07 18:56:31 +0700

    fix: REGISTER_SUCCESS

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 export const MESSAGE = {
   EMAIL_ALREADY_EXIST: 'Email is already registered',
-  REGISTER_SUCCESS: 'Register berhasil',
+  REGISTER_SUCCESS: 'Registrasi berhasil',
   LOGIN_SUCCESS: 'Login berhasil',
   LOGIN_FAILED: 'Email atau password salah',
   USER_NOT_FOUND: 'User tidak ditemukan',
